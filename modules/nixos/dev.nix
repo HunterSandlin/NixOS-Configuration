dfabf3885bd5c8ba6b5b3036c28c9c79db90080d
Hunter Sandlin <huntergsandlin@gmail.com>
2026-05-25 12:58:47 -0400

    Added rust overlay dynamically

--- modules/nixos/dev.nix
+++ modules/nixos/dev.nix
@@ -1,9 +1,10 @@
 # Development tools and services, designed for local development
-{ pkgs, lib, config, ... }:
+{ pkgs, lib, config, rust-overlay, ... }:
 {
   options.modules.dev.enable = lib.mkEnableOption "development tools";
 
   config = lib.mkIf config.modules.dev.enable {
+    nixpkgs.overlays = [ rust-overlay.overlays.default ];
     services.postgresql = {
       enable = true;
       ensureDatabases = [ "uhclone" ];
@@ -20,18 +21,16 @@
     };
 
     environment.systemPackages = with pkgs; [
+      (rust-bin.stable.latest.default.override {
+        extensions = [ "rust-src" "rust-analyzer" ];
+      })
+      rustlings
       neovim
       nodejs
       pnpm
       pipenv
       nodePackages.typescript
       nodePackages.typescript-language-server
-      cargo
-      rustc
-      rustlings
-      rust-analyzer
-      rustfmt
-      clippy
       postgresql
       (python3.withPackages (ps: with ps; [
         pip
@@ -44,7 +43,6 @@
       openssl
       fd
       ripgrep
-      gh
     ];
   };
 }
