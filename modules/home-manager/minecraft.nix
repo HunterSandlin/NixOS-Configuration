f1c8af484fcdd82018dc47d737ecfbd7c545c6fd
Hunter Sandlin <huntergsandlin@gmail.com>
2026-07-11 16:18:46 -0400

    Added unstabled version of jdk25

--- modules/home-manager/minecraft.nix
+++ modules/home-manager/minecraft.nix
@@ -6,8 +6,8 @@
     environment.systemPackages = [
       (pkgs.prismlauncher.override {
         # Extra Java runtimes beyond the default jdk21/jdk17/jdk8
-        jdks = with pkgs; [ jdk25 jdk21 jdk17 jdk8 ];
-
+        # unstable jdk25 since flake is pinned to nixos-25.05
+        jdks = (with pkgs; [ jdk21 jdk17 jdk8 ]) ++ [ pkgs-unstable.jdk25 ];    
         # Mods
         additionalPrograms = [ pkgs.ffmpeg ];
       })
